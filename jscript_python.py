14d79c86244cc9c30c94afdc11f41e9acbbaec95
Adenilson-Developers <adenilson.developers@gmail.com>
2022-06-13 18:07:07 -0300

    definin the problem

---
+++ jscript_python.py
@@ -0,0 +1,17 @@
+"""
+Prolema...
+Uma entrada de valores numéricos (Lista)
+
+3X -> javaScript - quando for multiplo de 3 - responde quero aprender javaScript
+5X -> Python - quando for multiplo de 5 - responde quero aprender python 
+3X e 5X javaScript e Python - quando for multiplo de 3 e 5 - quero aprender javaScript e python 
+
+ problem...
+
+ an input of numeric values ( List )
+
+ 3X -> javaScript - When it is multiple of 3 - say i want to learn JavaScrit
+ 5X -> Python - When it is multiple of 5  say i want to learn Python 
+ 3X e 5X javaScript and Python - When it is multiple of 3 and 5  - say i want to learn JavaScript and python 
+ 
+"""
